eda0f4f4a73acd355099921b9795ceec57d7632b
xraf12 <xraftis12@gmail.com>
2024-07-03 09:44:13 -0400

    fix flakev2

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -28,6 +28,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
+
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
